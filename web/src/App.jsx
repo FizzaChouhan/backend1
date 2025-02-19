d08b5bfc00a67530026f336ec8d9550c47f19df3
FizzaChouhan <chouhanfizza@gmail.com>
2025-02-19 22:53:31 +0500

    tired but trying

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -28,7 +28,7 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
-      <p>one more try again</p>
+      <p>one more try again and again</p>
 
     </>
   )
